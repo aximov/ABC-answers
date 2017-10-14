8c63619771737b1d9f661787e066c6b0d77459eb
aximov <deltalphan+github@gmail.com>
2017-10-14 12:40:08 +0900

    010 B

--- 010/B.cpp
+++ 010/B.cpp
@@ -6,11 +6,17 @@ int main()
   int n;
   cin >> n;
   int a;
-  
+  int sum = 0;
   for (int i = 0; i < n; ++i)
   {
     cin >> a;
-    
+    int mushiru = 0;
+    while ((a - mushiru) % 2 == 0 || (a - mushiru) % 3 == 2)
+    {
+      mushiru++;
+    }
+    sum += mushiru;
   }
+  cout << sum << endl;
   return 0;
 }
