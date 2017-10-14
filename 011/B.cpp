cdbdaf8137abdc805c8184ba90c9ee0b82397837
aximov <deltalphan+github@gmail.com>
2017-10-14 13:55:03 +0900

    011

---
+++ 011/B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  string S;
+  cin >> S;
+  S[0] = toupper(S[0]);
+  for (int i = 1; i < S.length(); ++i)
+    S[i] = tolower(S[i]);
+  cout << S << endl;
+  return 0;
+}
