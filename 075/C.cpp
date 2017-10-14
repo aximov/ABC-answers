3fc92c7c4d945170986028e0f5aced3eb72ca137
aximov <deltalphan+github@gmail.com>
2017-10-15 00:30:15 +0900

    あきらめた

--- 075/C.cpp
+++ 075/C.cpp
@@ -3,50 +3,60 @@ using namespace std;
 
 vector<int> G[50];
 int V, E;
+int s[50], t[50];
 bool flag[50] = {};
 
-bool dfs(int v){
-  if(G[v].size() == 0) return false;
+void dfs(int v)
+{
   // 頂点 v をみる
   flag[v] = true;
-  for(int i = 0; i < G[v].size(); ++i){
+  for (int i = 0; i < G[v].size(); ++i)
+  {
     // 隣接頂点をまだ見ていなければそこからみる
-    if(!flag[G[v][i]]) dfs(G[v][i]);
+    if (!flag[G[v][i]])
+      dfs(G[v][i]);
   }
-  // パス存在確認
-  for(int i = 0; i < V; ++i){
-    if(!flag[i]) return false;
-  }
-  return true;
 }
 
-int main(){
+int main()
+{
   cin >> V >> E;
 
-  for(int i = 0; i < E; ++i){
-    int s, t;
-    cin >> s >> t;
-    s--,t--;
-    G[s].push_back(t);
-    G[t].push_back(s);
+  for (int i = 0; i < E; ++i)
+  {
+    cin >> s[i] >> t[i];
+    s[i]--, t[i]--;
+    G[s[i]].push_back(t[i]);
+    G[t[i]].push_back(s[i]);
   }
 
-  int bridge = 0;
+  int ans = 0;
+  for (int i = 0; i < V; ++i)
+  {
+    // 訪問履歴初期化
+    for (int k = 0; k < 50; ++k) flag[k] = false;
 
-  for (int i = 0; i < V; ++i) {
-    for (int j = 0; j < G[i].size(); ++j){
-      int temp = 0, tempV = G[i][j];
-      for (int k = 0; k < G[tempV].size(); ++k){
-        if (G[tempV][k] == tempV) temp = k;
-      }
-      G[i].erase(G[i].begin() + j);
-      G[tempV].erase(G[tempV].begin() + temp);
-      if(!dfs(0)) bridge++; 
-      G[i].insert(G[i].begin() + j, tempV);
-      G[tempV].insert(G[tempV].begin() + temp, i);
-    }
+    // 辺を1つ消す
+    for (int k = 0; k < G[].size(); ++k)
+      if (G[dest][k] == i)
+        temp = k;
+    G[i].erase(G[i].begin() + j);
+    G[dest].erase(G[dest].begin() + temp);
+
+    dfs(0);
+
+    // パス存在確認
+    bool bridge = false;
+    for (int l = 0; l < V; ++l)
+      if (!flag[l])
+        bridge = true;
+    if (bridge)
+      ans++;
+
+    G[i].insert(G[i].begin() + j, dest);
+    G[dest].insert(G[dest].begin() + temp, i);
   }
 
-  cout << bridge << endl;
+  cout << ans << endl;
   return 0;
 }
